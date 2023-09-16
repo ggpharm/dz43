9b8a33b1ae28c0ba710edbb185c0c14e3d517bd5
SchuleR <i@iprub.ru>
2023-09-16 13:32:07 +0300

    Задание сделано

--- Program43.cs
+++ Program43.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
+// задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+
+Console.Write("Введите точку k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine(" ");
+
+Console.Write("Введите точку b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine(" ");
+
+Console.Write("Введите точку k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine(" ");
+
+Console.Write("Введите точку b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine(" ");
+
+Console.WriteLine($"Введено: {k1} {b1} {k2} {b2}");
+
+double x = 0;
+double y;
+if (k1 == k2)
+    Console.WriteLine("Линии не пересекаются");
+    else 
+    x = (b2 - b1) / (k1 - k2);
+    y = k1 * x + b1;
+
+    
+Console.Write($"Точка пересечения двух прямых: {x},{y}");
